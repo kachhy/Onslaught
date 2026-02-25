7d928c9063958f623fe01d7d45d45c43960c08a7
conman4562 <170995943+conman4562@users.noreply.github.com>
2026-02-25 18:30:46 -0500

    directional shifting

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -1,11 +1,51 @@
 #include "bitboard.h"
 
-int getBit(const BitBoard& bitboard, int bit) {
-    return bitboard & (BitBoard(1) << bit);
+inline BitBoard shiftNorth(const BitBoard& bitboard) {
+    return bitboard >> 8;
 }
 
-void setBit(BitBoard& bitboard, int square) {
+inline BitBoard shiftNorthEast(const BitBoard& bitboard) {
+    return (bitboard & ~H_FILE) >> 7;
+}
+
+inline BitBoard shiftEast(const BitBoard& bitboard) {
+    return (bitboard & ~H_FILE) << 1;
+}
+
+inline BitBoard shiftSouthEast(const BitBoard& bitboard) {
+    return (bitboard & ~H_FILE) << 9;
+}
+
+inline BitBoard shiftSouth(const BitBoard& bitboard) {
+    return bitboard << 8;
+}
+
+inline BitBoard shiftSouthWest(const BitBoard& bitboard) {
+    return (bitboard & ~A_FILE) << 7;
+}
+
+inline BitBoard shiftWest(const BitBoard& bitboard) {
+    return (bitboard & ~A_FILE) >> 1;
+}
+
+inline BitBoard shiftNorthWest(const BitBoard& bitboard) {
+    return (bitboard & ~A_FILE) >> 9;
+}
+
+inline int getBit(const BitBoard& bitboard, int bit) {
+    return static_cast<int>((bitboard >> bit) & BitBoard(1));
+}
+
+inline void setBit(BitBoard& bitboard, int square) {
     bitboard |= (BitBoard(1) << square);
+}
+
+inline int getRank(int square) {
+    return square >> 3;
+}
+
+inline int getFile(int square) {
+    return square & 7;
 }
 
 void printBitboard(const BitBoard& bitboard) {
--- src/bitboard.h
+++ src/bitboard.h
@@ -6,6 +6,33 @@
 
 typedef uint64_t BitBoard;
 
+#define A_FILE 0x0101010101010101ULL
+#define B_FILE 0x0202020202020202ULL
+#define C_FILE 0x0404040404040404ULL
+#define D_FILE 0x0808080808080808ULL
+#define E_FILE 0x1010101010101010ULL
+#define F_FILE 0x2020202020202020ULL
+#define G_FILE 0x4040404040404040ULL
+#define H_FILE 0x8080808080808080ULL
+
+#define RANK_1 0xFF00000000000000ULL
+#define RANK_2 0x00FF000000000000ULL
+#define RANK_3 0x0000FF0000000000ULL
+#define RANK_4 0x000000FF00000000ULL
+#define RANK_5 0x00000000FF000000ULL
+#define RANK_6 0x0000000000FF0000ULL
+#define RANK_7 0x000000000000FF00ULL
+#define RANK_8 0x00000000000000FFULL
+
+BitBoard shiftNorth(const BitBoard& bitboard);
+BitBoard shiftNorthEast(const BitBoard& bitboard);
+BitBoard shiftEast(const BitBoard& bitboard);
+BitBoard shiftSouthEast(const BitBoard& bitboard);
+BitBoard shiftSouth(const BitBoard& bitboard);
+BitBoard shiftSouthWest(const BitBoard& bitboard);
+BitBoard shiftWest(const BitBoard& bitboard);
+BitBoard shiftNorthWest(const BitBoard& bitboard);
+
 int getBit(const BitBoard& bitboard, int bit);
 void setBit(BitBoard& bitboard, int square);
 void printBitboard(const BitBoard& bitboard);
