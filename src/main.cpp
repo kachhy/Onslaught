12cc0b229248b86ac9926914c6f1a9c373c86438
kachhy <109984629+kachhy@users.noreply.github.com>
2026-02-25 15:03:19 -0500

    Improve test documentation and output

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include "board.h"
 
 int main() {
-    // Tests
+    // Board tests
     Board b; // ideally default position
+    std::cout << "Starting position:\n";
     b.printBoard();
+
+    std::cout << "Empty board:\n";
     b.clear();
     b.printBoard(); // Should be empty
 
+    std::cout << "FEN Position 1:\n";
     b.loadFEN("r1b1r1k1/1pp2ppp/2n5/p1bqp3/8/P2P1NP1/1P2PPBP/R1BQ1RK1 w - - 0 11"); // Should now be some roughly even position
     b.printBoard();
     
