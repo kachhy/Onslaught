d471262c7ccb499dd135b27bbe75b5ccf885f6c2
kachhy <109984629+kachhy@users.noreply.github.com>
2026-03-15 19:20:46 -0400

    Add basic piece square table evaluation

--- src/eval.cpp
+++ src/eval.cpp
@@ -9,10 +9,111 @@ int material_values[6] = {
     0, // KING (shouldn't hit this case ever)
 };
 
-int eval(const Board& board) {
-    int score = 0;
+constexpr int pst[7][64] = {
+    // pawn
+    {
+        0,  0,  0,  0,  0,  0,  0,  0, // Rank 1
+        5, 10, 10,-20,-20, 10, 10,  5, // Rank 2
+        5, -5,-10,  0,  0,-10, -5,  5, // Rank 3
+        0,  0,  0, 20, 20,  0,  0,  0, // Rank 4
+        5,  5, 10, 25, 25, 10,  5,  5, // Rank 5
+        10, 10, 20, 30, 30, 20, 10, 10, // Rank 6
+        50, 50, 50, 50, 50, 50, 50, 50, // Rank 7
+        0,  0,  0,  0,  0,  0,  0,  0  // Rank 8
+    },
+    // knight
+    {
+        -50,-40,-30,-30,-30,-30,-40,-50,
+        -40,-20,  0,  5,  5,  0,-20,-40,
+        -30,  5, 10, 15, 15, 10,  5,-30,
+        -30,  0, 15, 20, 20, 15,  0,-30,
+        -30,  5, 15, 20, 20, 15,  5,-30,
+        -30,  0, 10, 15, 15, 10,  0,-30,
+        -40,-20,  0,  0,  0,  0,-20,-40,
+        -50,-40,-30,-30,-30,-30,-40,-50
+    },
+    // bishop
+    {
+        -20,-10,-10,-10,-10,-10,-10,-20,
+        -10,  5,  0,  0,  0,  0,  5,-10,
+        -10, 10, 10, 10, 10, 10, 10,-10,
+        -10,  0, 10, 10, 10, 10,  0,-10,
+        -10,  5,  5, 10, 10,  5,  5,-10,
+        -10,  0,  5, 10, 10,  5,  0,-10,
+        -10,  0,  0,  0,  0,  0,  0,-10,
+        -20,-10,-10,-10,-10,-10,-10,-20
+    },
+    // rook
+    {
+            0,  0,  0,  5,  5,  0,  0,  0,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+        -5,  0,  0,  0,  0,  0,  0, -5,
+            5, 10, 10, 10, 10, 10, 10,  5,
+            0,  0,  0,  0,  0,  0,  0,  0
+    },
+    // queen
+    {
+        -20,-10,-10, -5, -5,-10,-10,-20,
+        -10,  0,  5,  0,  0,  0,  0,-10,
+        -10,  5,  5,  5,  5,  5,  0,-10,
+        0,  0,  5,  5,  5,  5,  0, -5,
+        -5,  0,  5,  5,  5,  5,  0, -5,
+        -10,  0,  5,  5,  5,  5,  0,-10,
+        -10,  0,  0,  0,  0,  0,  0,-10,
+        -20,-10,-10, -5, -5,-10,-10,-20
+    },
+    // king_mg
+    {
+        20, 30, 10,  0,  0, 10, 30, 20,
+        20, 20,  0,  0,  0,  0, 20, 20,
+        -10,-20,-20,-20,-20,-20,-20,-10,
+        -20,-30,-30,-40,-40,-30,-30,-20,
+        -30,-40,-40,-50,-50,-40,-40,-30,
+        -30,-40,-40,-50,-50,-40,-40,-30,
+        -30,-40,-40,-50,-50,-40,-40,-30,
+        -30,-40,-40,-50,-50,-40,-40,-30
+    },
+    // king_eg
+    {
+        -50,-30,-30,-30,-30,-30,-30,-50,
+        -30,-30,  0,  0,  0,  0,-30,-30,
+        -30,-10, 20, 30, 30, 20,-10,-30,
+        -30,-10, 30, 40, 40, 30,-10,-30,
+        -30,-10, 30, 40, 40, 30,-10,-30,
+        -30,-10, 20, 30, 30, 20,-10,-30,
+        -30,-20,-10,  0,  0,-10,-20,-30,
+        -50,-40,-30,-20,-20,-30,-40,-50
+    }
+};
 
-    score += material_values[PAWN] * bitCount(board.getPieceBB(WHITE_PAWN));
+static int applyPST(const Board& board, const DefaultPiece piece) {
+    int score = 0;
+    BitBoard white_piece_bb = board.getPieceBB(makePiece(piece, WHITE));
+    BitBoard black_piece_bb = board.getPieceBB(makePiece(piece, BLACK));
+    while (white_piece_bb) {
+        score += pst[piece][popLSB(white_piece_bb)];
+    }
+    while (black_piece_bb) {
+        score -= pst[piece][flipRank(popLSB(black_piece_bb))];
+    }
+    return score;
+}
+
+static int applyAllPST(const Board& board) {
+    int score = applyPST(board, PAWN);
+    score += applyPST(board, KNIGHT);
+    score += applyPST(board, BISHOP);
+    score += applyPST(board, ROOK);
+    score += applyPST(board, QUEEN);
+    score += applyPST(board, KING);
+    return score;
+}
+
+int eval(const Board& board) {
+    int score = material_values[PAWN] * bitCount(board.getPieceBB(WHITE_PAWN));
     score += material_values[KNIGHT] * bitCount(board.getPieceBB(WHITE_KNIGHT));
     score += material_values[BISHOP] * bitCount(board.getPieceBB(WHITE_BISHOP));
     score += material_values[ROOK] * bitCount(board.getPieceBB(WHITE_ROOK));
@@ -24,5 +125,7 @@ int eval(const Board& board) {
     score += material_values[ROOK] * -1 * bitCount(board.getPieceBB(BLACK_ROOK));
     score += material_values[QUEEN] * -1 * bitCount(board.getPieceBB(BLACK_QUEEN));
 
-    return score;
+    score += applyAllPST(board);
+
+    return board.getSTM() == WHITE ? score : -score;
 }
