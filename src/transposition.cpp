22380e2e0a903c461cbcbf836f81df97e799d883
kachhy <109984629+kachhy@users.noreply.github.com>
2026-03-16 14:10:57 -0400

    TT Fixes and improved code styling

--- src/transposition.cpp
+++ src/transposition.cpp
@@ -8,29 +8,32 @@ TTable::TTable() : table_age(0) {
 
 void TTable::insert(const Board& board, Move best_move, int32_t score, TTBound bound, size_t depth) {
     const uint64_t hash = board.hash() % TABLE_SIZE;
-
-    if (table[hash].count < 3) {
+    EntryTriple& bucket = table[hash];
+    if (bucket.count < 3) {
         // Ensure entry doesn't already exist
-        for (uint8_t i = 0; i < table[hash].count; i++) {
-            if (table[hash].entries[i].hash == board.hash()) {
+        for (uint8_t i = 0; i < bucket.count; i++) {
+            if (bucket.entries[i].hash == board.hash()) {
+                if (depth >= bucket.entries[i].depth || bound == EXACTBOUND) {
+                    bucket.entries[i] = {board.hash(), best_move, score, bound, depth, table_age};
+                }
                 return;
             }
         }
 
-        table[hash].entries[table[hash].count] = {board.hash(), best_move, score, bound, depth, table_age};
-        table[hash].count++;
+        bucket.entries[bucket.count] = {board.hash(), best_move, score, bound, depth, table_age};
+        bucket.count++;
         table_size++; // We added a new entry
         return;
     }
 
     // Eviction policy
-    Entry& best_kickout        = table[hash].entries[0];
-    int64_t best_kickout_score = table[hash].entries[0].depth - (table_age - table[hash].entries[0].last_seen);
+    Entry& best_kickout        = bucket.entries[0];
+    int64_t best_kickout_score = bucket.entries[0].depth - (table_age - bucket.entries[0].last_seen);
 
-    for (uint8_t i = 1; i < table[hash].count; i++) {
-        const int64_t this_kickout_score = table[hash].entries[i].depth - (table_age - table[hash].entries[i].last_seen);
+    for (uint8_t i = 1; i < bucket.count; i++) {
+        const int64_t this_kickout_score = bucket.entries[i].depth - (table_age - bucket.entries[i].last_seen);
         if (this_kickout_score < best_kickout_score) {
-            best_kickout       = table[hash].entries[i];
+            best_kickout       = bucket.entries[i];
             best_kickout_score = this_kickout_score;
         }
     }
@@ -40,18 +43,19 @@ void TTable::insert(const Board& board, Move best_move, int32_t score, TTBound b
 
 bool TTable::fetch(const Board& board, Entry& entry) {
     const uint64_t hash = board.hash() % TABLE_SIZE;
+    EntryTriple& bucket = table[hash];
     table_age++;
 
-    if (!table[hash].count) {
+    if (!bucket.count) {
         return false;
     }
 
     // Check each hot element
     bool found = false;
-    for (uint8_t i = 0; i < table[hash].count; i++) {
-        table[hash].entries[i].last_seen = table_age;
-        if (table[hash].entries[i].hash == board.hash()) {
-            entry = table[hash].entries[i];
+    for (uint8_t i = 0; i < bucket.count; i++) {
+        if (bucket.entries[i].hash == board.hash()) {
+            entry = bucket.entries[i];
+            bucket.entries[i].last_seen = table_age;
             found = true;
         }
     }
