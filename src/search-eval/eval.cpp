5dfba90e7ed307e37ce34da6df19ac0fd20a5553
kachhy <109984629+kachhy@users.noreply.github.com>
2026-04-20 14:39:24 -0400

    Remove redundant blocking check

--- src/search-eval/eval.cpp
+++ src/search-eval/eval.cpp
@@ -294,7 +294,7 @@ static inline Score evaluateBishops(const PieceCounts& pc, const Board& board, c
         score += blocking_pawns * BAD_BISHOP;
         TRACE_ADD(bad_bishop, WHITE, blocking_pawns);
 
-        if (valid_moves && !safe_moves) {
+        if (safe_moves) {
             score += TRAPPED_BISHOP;
             TRACE_INC(trapped_bishop, WHITE);
         }
@@ -314,7 +314,7 @@ static inline Score evaluateBishops(const PieceCounts& pc, const Board& board, c
         score -= blocking_pawns * BAD_BISHOP;
         TRACE_ADD(bad_bishop, BLACK, blocking_pawns);
 
-        if (valid_moves && !safe_moves) {
+        if (!safe_moves) {
             score -= TRAPPED_BISHOP;
             TRACE_INC(trapped_bishop, BLACK);
         }
