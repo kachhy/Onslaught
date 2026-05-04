e3c6e1d16ac2c81471dd4a550eaafa76df85886c
conman4562 <170995943+conman4562@users.noreply.github.com>
2026-05-04 02:44:42 -0400

    testing new params for singular extensions

--- src/search-eval/search.cpp
+++ src/search-eval/search.cpp
@@ -344,7 +344,7 @@ int search(
         int extension = 0;
         if (excluded_move == NO_MOVE && move == tt_entry.best_move && !in_check && tt_hit && depth >= 8 && tt_entry.depth >= depth - 3 && tt_entry.bound != UPPERBOUND && std::abs(tt_entry.score) < SCORE_MAX - MAX_GAME_MOVES) {
             int singular_beta = tt_entry.score - SE_MARGIN * depth;
-            int singular_depth = (depth - 1) / 2;
+            int singular_depth = (depth) / 4;
             int singular_score = search(board, singular_depth, singular_beta - 1, singular_beta, hard_cap, max_nodes, start, ply, false, pv_table, max_ply, move);
             
             if (singular_score < singular_beta) {
