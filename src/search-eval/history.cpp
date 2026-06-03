c95d46b06c540e48730e0bd2713422d25d8dfa98
kachhy <109984629+kachhy@users.noreply.github.com>
2026-06-03 17:43:32 -0400

    Conthist framework

--- src/search-eval/history.cpp
+++ src/search-eval/history.cpp
@@ -26,3 +26,19 @@ void updateScoreHistory(int depth, Side stm, Move move, const MoveList& quiets_t
 int getScoreHistory(Side stm, Move move) {
     return score_history[stm][From(move)][To(move)];
 }
+
+void updateContHistory(int depth, Side stm, Move move, Move prev_move, const MoveList& quiets_tried) {
+    const int bonus = std::min(depth * depth, MAX_HISTORY);
+    auto& h = cont_hist[stm][Piece(prev_move)][To(prev_move)][Piece(move)][To(move)];
+    h += bonus - (h * std::abs(bonus) / MAX_HISTORY);
+
+    // malus: penalize all quiet moves searched before this cutoff
+    for (const Move quiet_move : quiets_tried) {
+        if (quiet_move == move) { // We just gave this move a bonus
+            continue;
+        }
+        
+        auto& h = cont_hist[stm][Piece(prev_move)][To(prev_move)][Piece(quiet_move)][To(quiet_move)];
+        h += -bonus - (h * std::abs(bonus) / MAX_HISTORY);
+    }
+}
